73ff6816393f3adcba0e7416965e58f1006d41be
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-12-18 21:36:06 +0530

    created a langraph pipeline

--- langraph_learn/chat.py
+++ langraph_learn/chat.py
@@ -1,7 +1,16 @@
+from dotenv import load_dotenv
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph,START,END
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+llm=init_chat_model(
+    model="gemini-2.5-flash",
+    model_provider="google_genai"
+)
 
 
 class State(TypedDict):
@@ -10,11 +19,13 @@ class State(TypedDict):
 
 #This is a node which return below messages
 def chatbot(state:State):
-    print("Inside chatbot node",state)
-    return {"messages":["Hi,This is a message from ChatBot Node"]}
+    #print("\n\n Inside chatbot node",state)
+    response=llm.invoke(state.get("messages"))
+    #return {"messages":["Hi,This is a message from ChatBot Node"]}
+    return {"messages":[response]}
 
 def samplenode(state:State):
-    print("Inside samplenode node",state)
+    print("\n\nInside samplenode node",state)
     return {"messages":["Sample Message Appended"]}
 
 graph_builder=StateGraph(State)
@@ -31,3 +42,6 @@ graph_builder.add_edge("chatbot","samplenode")
 graph_builder.add_edge("samplenode",END)
 
 graph =graph_builder.compile()
+
+updated_state=graph.invoke(State({"messages":["Hi,My name is Piyush Garg"]}))
+print("\n\n updated_state",updated_state)
